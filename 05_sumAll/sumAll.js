6d569772282225735f7fac00eb83cadd2aa99b1a
Shin <crypticshin@gmail.com>
2023-06-20 17:22:31 +0100

    add logic
    return ERROR if either min or max is not a number
    return ERROR if either min or max is negative
    rearrange if max is less than min

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,11 @@
 const sumAll = function(min, max) {
+    if(!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if(min < 0 || max < 0) return "ERROR";
+    if(max < min) {
+        let temp = min;
+        min = max;
+        max = temp; 
+    }
     let sum = 0;
     for(let i = min; i < max + 1; i++) {
         sum += i;
@@ -6,7 +13,7 @@ const sumAll = function(min, max) {
     return sum;
 };
 
-console.log(sumAll(1, 4))
+console.log(sumAll(10, "90"))
 
 // Do not edit below this line
 module.exports = sumAll;
