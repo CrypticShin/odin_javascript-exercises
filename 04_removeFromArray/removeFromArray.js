86d5c2454258175551b3690ccd5c37fb35b499ca
Shin <crypticshin@gmail.com>
2023-06-20 16:34:50 +0100

    Update removeFromArray()
    removeFromArray() takes in an array and an item
    removes the item from the array, and
    returns the array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, item) {
+    for(i in array) {
+        if(i == item) {
+            array.splice(array.indexOf(item), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
